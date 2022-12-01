f12533d45bfc02cc93f4c38be89c5bc29013c71e
Matteo Tafani Alunno <matteo.tafanialunno@gmail.com>
2022-12-01 08:49:15 +0100

    Adding typings to use it in typescript

---
+++ types/runtime.d.ts
@@ -0,0 +1,10 @@
+/**
+ * This is a macro replaced at buildtime with the "String" representation of
+ * `symbol`
+ *
+ * This macro allow to preserve the string representation of the symbol after mangling and uglyfications
+ *
+ * @param {*} symbol
+ * @return string
+ */
+export function BUILDTIME_SYMBOL_TO_STRING(symbol: any): string;
